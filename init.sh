573f282411869df724cba89ee6301b4480a4fe57
EC2 Default User <ec2-user@ip-172-31-10-74.eu-central-1.compute.internal>
2015-11-09 02:19:44 +0000

    Added my clone command

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ sudo yum install -y git puppet3
 sudo puppet module install puppetlabs-vcsrepo
 sudo puppet resource yumrepo > initialrepos.pp
 sudo mv /etc/yum.repos.d/* /tmp/
+git clone https://github.com/Cinderhaze/s3-repo-sandbox
 sudo puppet apply ~/s3-repo-sandbox/s3_plugin.pp
 yum search epel
 #restore the repos with this command, possibly remove the custom repo first
